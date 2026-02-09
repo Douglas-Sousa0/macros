75f1b3e69e0f86daf18be07fb6e6e6826d74234d
douglas <douglas.sousa16a@gmail.com>
2026-02-09 17:42:17 -0300

    Adicionando funcionalidade de cadastrar variantes para o texto da macro (até 5)

--- src/pages/CadastroMacros/index.js
+++ src/pages/CadastroMacros/index.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useReducer } from 'react'
 import { database } from '../../firebaseConnection'
 import { collection, addDoc } from 'firebase/firestore'
 import './cadastro_macros.css'
@@ -7,44 +7,87 @@ function CadastroMacros(){
     const [titulo, setTitulo] = useState('')
     const [texto, setTexto] = useState('')
 
-   /*  const [addVariantes, setAddVariantes] = useState(false)
-    const [qtdVariantes, setQtdVariantes] = useState(0)
- */
-    // let lista_variantes = []
+    const [addVariantes, setAddVariantes] = useState(false)
+    const [qtdVariantes, setQtdVariantes] = useState(1)
+    
+    const [variantes, dispatch] = useReducer(atualizar_variantes, {
+        variante_1: '',
+        variante_2: '',
+        variante_3: '',
+        variante_4: '',
+        variante_5: ''
+    })
+
+    let lista_variantes = []
 
     async function cadastrar_macro(e){
         e.preventDefault()
 
         const ref = collection(database, 'macros')
 
-        await addDoc(ref, {
+        let campos = {
             titulo: titulo,
-            texto: texto
-        })
+            texto: texto,
+            possuiVariantes: addVariantes
+        }
+
+        if(addVariantes){
+            campos.variantes = {}
+            
+            for(let contador = 0; contador < qtdVariantes; contador++){
+                campos.variantes[`variante_${contador + 1}`] = variantes[`variante_${contador + 1}`]
+            }
+        }
+            
+        await addDoc(ref, campos)
         .then(() => {
             setTitulo('')
             setTexto('')
+            dispatch({ type: 'LIMPAR_TUDO'})
         })
         .catch(erro => {
             console.log(erro)
         })
     }
 
-    /* for(let contador = 0; contador < qtdVariantes; contador++){
+    function atualizar_variantes(variantes, acao){
+        switch (acao.type){
+            case 'VARIANTE_1':
+                return { ...variantes, variante_1: acao.payload }
+
+            case 'VARIANTE_2':
+                return { ...variantes, variante_2: acao.payload }
+            
+            case 'VARIANTE_3':
+                return { ...variantes, variante_3: acao.payload }
+            
+            case 'VARIANTE_4':
+                return { ...variantes, variante_4: acao.payload }
+            
+            case 'VARIANTE_5':
+                return { ...variantes, variante_5: acao.payload}
+
+            case 'LIMPAR_TUDO':
+                return { variante_1: '', variante_2: '', variante_3: '', variante_4: '', variante_5: ''}
+        }
+    }
+
+
+    for(let contador = 0; contador < qtdVariantes; contador++){
         lista_variantes.push(
-            <textarea 
-            placeholder='Digite um texto'
-            key={contador}/>
-        )
-    } */
+            <>
+                <label>Variante {contador + 1}</label>
+                <textarea 
+                placeholder='Digite um texto'
+                required
+                value={variantes[`variante_${contador + 1}`]}
+                onChange={e => dispatch({ type: `VARIANTE_${contador + 1}`, payload: e.target.value})}/>
+            </>
+        )}
 
-    /* useEffect(() => {
-
-    }, [qtdVariantes])
- */
     return(
         <div className='cadastro-macros'>
-            <form onSubmit={cadastrar_macro}>
+            <form onSubmit={ cadastrar_macro }>
                 <label htmlFor='titulo'>Título</label>
                 
                 <input 
@@ -54,9 +97,10 @@ function CadastroMacros(){
                 onChange={e => setTitulo(e.target.value)}
                 name='titulo'
                 id='titulo'
-                autoComplete='off'/>
+                autoComplete='off'
+                required/>
                 
-                {/* <label>
+                <label>
                     <input 
                     type='checkbox'
                     checked={addVariantes}
@@ -70,10 +114,11 @@ function CadastroMacros(){
                     type='number'
                     min={1}
                     max={5}
-                    defaultValue={1}
-                    onChange={e => setQtdVariantes(e.target.value)}/>
+                    value={qtdVariantes}
+                    onChange={e => setQtdVariantes(e.target.value)}
+                    />
                 </label>
-                } */}
+                }
                 
                 <label htmlFor='texto'>Texto</label> 
                 
@@ -82,10 +127,12 @@ function CadastroMacros(){
                 value={texto}
                 onChange={e => setTexto(e.target.value)}
                 name='texto'
-                id='texto'/>
-{/* 
-                {lista_variantes}
- */}
+                id='texto'
+                required/>
+
+                {addVariantes &&
+                lista_variantes}
+
                 <button type='submit'>Cadastrar</button>
             </form>
         </div>
